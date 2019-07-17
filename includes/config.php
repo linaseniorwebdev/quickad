b278dd08a6c831f72ed019364fd2111f61f45451
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-07-17 17:51:28 +0900

    Some Update

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'quickad';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'mysql';
+$config['db']['pre'] = 'ad_';
+
+$config['version'] = '8.1';
+$config['installed'] = '1';
